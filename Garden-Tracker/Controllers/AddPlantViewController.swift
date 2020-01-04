b87ddd1fc78da4f7e7e8c88d6560d43419abaaa6
Hallie <idhallie@gmail.com>
2020-01-04 12:27:24 -0800

    Setup AddPlantVC with segue.

---
+++ Garden-Tracker/Controllers/AddPlantViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddPlantViewController.swift
+//  Garden-Tracker
+//
+//  Created by Hallie Johnson on 1/4/20.
+//  Copyright © 2020 Hallie Johnson. All rights reserved.
+//
+
+import UIKit
+
+class AddPlantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
