e548ce89c70fe6e090c534896c80b1385bc0a9ba
Hallie <idhallie@gmail.com>
2020-01-04 13:38:01 -0800

    Can add a plant name to main screen.

--- Garden-Tracker/Controllers/AddPlantViewController.swift
+++ Garden-Tracker/Controllers/AddPlantViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class AddPlantViewController: UIViewController {
 
+    @IBOutlet weak var plantName: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func addBtnTapped(_ sender: UIButton) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let plant = Plant(context: context)
+        plant.name = plantName.text!
+        
+        //Save the data to coredata
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+    
 }
