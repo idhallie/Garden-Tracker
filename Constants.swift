d1389d4f9bfcc3d5adb1db6adf979d80e50e35be
Hallie <idhallie@gmail.com>
2020-01-05 15:31:59 -0800

    Created Constants file with api key and added it to gitignore.

---
+++ Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Garden-Tracker
+//
+//  Created by Hallie Johnson on 1/5/20.
+//  Copyright © 2020 Hallie Johnson. All rights reserved.
+//
+
+import Foundation
+
+let trefleApiKey = "eHltbFF6REV2NzZVSHVmdHNRQUNLQT09"
