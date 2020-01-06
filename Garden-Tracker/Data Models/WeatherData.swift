c4fa05480e33b03bd67cde180e7e80135925c557
Hallie <idhallie@gmail.com>
2020-01-06 14:06:46 -0800

    Obtains temperature and condition ID and returns the name of file for icon.

---
+++ Garden-Tracker/Data Models/WeatherData.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherData.swift
+//  Garden-Tracker
+//
+//  Created by Hallie Johnson on 1/6/20.
+//  Copyright © 2020 Hallie Johnson. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let cod: String
+    let list: [List]
+    
+}
+
+struct List: Decodable {
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+}
--- Garden-Tracker/Data Models/WeatherManager.swift
+++ Garden-Tracker/Data Models/WeatherManager.swift
@@ -13,6 +13,42 @@ struct WeatherManager {
     
     func fetchWeather(latitude: Double, longitude: Double) {
         let urlString = "\(weatherURL)lat=\(latitude)&lon=\(longitude)&appid=\(weatherApiKey)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodeData = try decoder.decode(WeatherData.self, from: weatherData)
+            let temp = decodeData.list[0].main.temp
+            let id = decodeData.list[0].weather[0].id
+        
+            let weather = WeatherModel(conditionId: id, temperature: temp)
+            
+            print(weather.conditionName)
+        } catch {
+            print(error)
+        }
+        
+    }
+
 }
