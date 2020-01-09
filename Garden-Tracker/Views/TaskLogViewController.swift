7045e8979d562bd2c18ed3dcbc2d39c2ff67c0f4
Hallie <idhallie@gmail.com>
2020-01-09 10:12:58 -0800

    Laid out storyboards, customized tab bar colors, adjusted Add Plant button.

---
+++ Garden-Tracker/Views/TaskLogViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TaskLogViewController.swift
+//  Garden-Tracker
+//
+//  Created by Hallie Johnson on 1/9/20.
+//  Copyright © 2020 Hallie Johnson. All rights reserved.
+//
+
+import UIKit
+
+class TaskLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
