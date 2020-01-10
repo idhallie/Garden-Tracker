c047b83a3d843b50cdc7dca6107f32a082597fd9
Hallie <idhallie@gmail.com>
2020-01-09 17:55:00 -0800

    Plant menu drops down with hard coded items.

--- Garden-Tracker/Views/AddTaskViewController.swift
+++ Garden-Tracker/Views/AddTaskViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import CoreData
 
 class AddTaskViewController: UIViewController {
 
+    
+    @IBOutlet weak var plantMenuTitle: UIButton!
+    @IBOutlet weak var tblView: UITableView!
+    
+    var plants: [Plant] = []
+    var plantList = ["Rose", "Daisy", "Sunflower"]
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tblView.isHidden = true
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func onClickDropBtn(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            self.tblView.isHidden = !self.tblView.isHidden
+            self.view.layoutIfNeeded()
+        }
+    }
+}
+
+extension AddTaskViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return plantList.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "plantCell", for: indexPath)
+        
+        
+        
+        cell.textLabel?.text = plantList[indexPath.row]
 
+        return cell
+    }
+    
+    
 }
