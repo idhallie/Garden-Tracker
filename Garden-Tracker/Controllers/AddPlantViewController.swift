4760206826f92ddf117f779bf3fdbde8ad4d9ba7
Hallie <idhallie@gmail.com>
2020-01-13 21:27:18 -0800

    Can add a photo from library or camera to Add Plant page.

--- Garden-Tracker/Controllers/AddPlantViewController.swift
+++ Garden-Tracker/Controllers/AddPlantViewController.swift
@@ -9,19 +9,8 @@
 import UIKit
 import CoreData
 
-extension UIViewController {
-    func HideKeyboard() {
-        let Tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DismissKeyboard))
-        
-        view.addGestureRecognizer(Tap)
-    }
-    
-    @objc func DismissKeyboard() {
-        view.endEditing(true)
-    }
-}
 
-class AddPlantViewController: UIViewController, UITextViewDelegate {
+class AddPlantViewController: UIViewController, UITextViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var plants = [Plant]()
     @IBOutlet weak var plantName: UITextField!
@@ -40,6 +29,11 @@ class AddPlantViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var floweringMenuTitle: UIButton!
     @IBOutlet var floweringButtons: [UIButton]!
     var flowering : String! = ""
+    
+    // Add Image
+    @IBOutlet weak var imageView: UIImageView!
+    
+
     
     // Notes field
     @IBOutlet weak var plantNotes: UITextView!
@@ -103,7 +97,7 @@ class AddPlantViewController: UIViewController, UITextViewDelegate {
     }
     
     
-    // MARK: Add new plant
+    // MARK: - Add new plant
     @IBAction func addBtnTapped(_ sender: UIButton) {
 
         
@@ -130,4 +124,55 @@ class AddPlantViewController: UIViewController, UITextViewDelegate {
             })
         }
     }
+    
+    @IBAction func addPhotoClicked(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action:UIAlertAction) in imagePickerController.sourceType = .camera
+
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+              self.present(imagePickerController, animated: true, completion: nil)
+            } else {
+                print("Camera not available.")
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action:UIAlertAction) in imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        imageView.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
+
+extension UIViewController {
+    func HideKeyboard() {
+        let Tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DismissKeyboard))
+        
+        view.addGestureRecognizer(Tap)
+    }
+    
+    @objc func DismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
