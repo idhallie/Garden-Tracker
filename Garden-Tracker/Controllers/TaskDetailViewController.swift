d4516f5e3b7b85b0fb06d5921d3b37bbb4078f88
Hallie <idhallie@gmail.com>
2020-01-15 20:38:16 -0800

    Fixed indentation on detail page and font colors.

---
+++ Garden-Tracker/Controllers/TaskDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskDetailViewController.swift
+//  Garden-Tracker
+//
+//  Created by Hallie Johnson on 1/15/20.
+//  Copyright © 2020 Hallie Johnson. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
