7020c401d716623aa4156c03732d4511cb23a764
Hallie <idhallie@gmail.com>
2020-01-15 18:35:59 -0800

    Can now delete tasks in Task Log.

--- Garden-Tracker/Views/TaskLogViewController.swift
+++ Garden-Tracker/Views/TaskLogViewController.swift
@@ -13,6 +13,7 @@ class TaskLogViewController: UIViewController, UITableViewDataSource, UITableVie
     
     @IBOutlet weak var tableView: UITableView!
     var activities : [Activity] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,14 +41,28 @@ class TaskLogViewController: UIViewController, UITableViewDataSource, UITableVie
         return cell
     }
     
-    func loadActivites() {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+        if editingStyle == .delete {
+            let activity = activities[indexPath.row]
+            context.delete(activity)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+
+            do {
+               activities = try context.fetch(Activity.fetchRequest())
+            }
+            catch {
+                print("Fetching failed \(error)")
+            }
+        }
+        tableView.reloadData()
+    }
     
+    func loadActivites() {
         do {
         activities = try context.fetch(Activity.fetchRequest())
         } catch {
             print("Error loading activities: \(error)")
         }
     }
-
 }
