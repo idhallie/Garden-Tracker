d3ec051b8b0f1b5923fa98f8216b59c6418cd69a
Hallie <idhallie@gmail.com>
2020-01-20 20:56:05 -0800

    Finally fixed the table view dropdown background color in Add Task.

--- Garden-Tracker/Views/AddTaskViewController.swift
+++ Garden-Tracker/Views/AddTaskViewController.swift
@@ -16,7 +16,7 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     @IBOutlet weak var plantMenuTitle: UIButton!
-    @IBOutlet weak var tblView: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     var selectedPlant : Plant?
     
     // Task menu outlets
@@ -42,10 +42,10 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
         addTaskButton.createFloatingActionButton()
         
         
-        tblView.dataSource = self
-        tblView.delegate = self
-        tblView.rowHeight = 50
-        tblView.isHidden = true
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = 50
+        tableView.isHidden = true
         
         // Notes defaults
         taskNotes.text = "Notes"
@@ -55,7 +55,7 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
 
     @IBAction func onClickDropBtn(_ sender: UIButton) {
         UIView.animate(withDuration: 0.3) {
-            self.tblView.isHidden = !self.tblView.isHidden
+            self.tableView.isHidden = !self.tableView.isHidden
             self.view.layoutIfNeeded()
         }
     }
@@ -65,7 +65,7 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
         loadPlants()
 
         // reload the table view
-        tblView.reloadData()
+        tableView.reloadData()
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -74,19 +74,20 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let plantCell = UITableViewCell()
-  
         let plant = plants[indexPath.row]
-        plantCell.textLabel?.text = plant.name!
+        print(plant)
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = plant.name!
 
-        return plantCell
+        return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedPlant = plants[indexPath.row]
         plantMenuTitle.setTitle("Plant: \(plants[indexPath.row].name!)", for: .normal)
         UIView.animate(withDuration: 0.3) {
-         self.tblView.isHidden = !self.tblView.isHidden
+         self.tableView.isHidden = !self.tableView.isHidden
          self.view.layoutIfNeeded()
          }
     }
